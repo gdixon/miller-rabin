c4d0c843d9e8ca2cecf755e4b40d41781a917a28
Graham Dixon <graham@gdixon.co.uk>
2021-09-09 17:09:26 +0100

    mr as es6 module

--- test/api-test.js
+++ test/api-test.js
@@ -1,11 +1,14 @@
-var assert = require('assert');
-var mr = require('../').create();
-var bn = require('bn.js');
+import assert from 'assert';
+
+import mr from '../';
+import bn from 'bn.js';
+
+const miller = mr.create();
 
 describe('Miller-Rabin', function() {
   it('should test number for primality', function() {
-    assert(!mr.test(new bn(221)));
-    assert(mr.test(new bn(257)));
+    assert(!miller.test(new bn(221)));
+    assert(miller.test(new bn(257)));
 
     var p = new bn('dba8191813fe8f51eaae1de70213aafede8f323f95f32cff' +
                    '8b64ebada275cfb18a446a0150e5fdaee246244c5f002ce0' +
@@ -13,6 +16,6 @@ describe('Miller-Rabin', function() {
                    '052774338d3310a6e020c46168cb1f94014e9312511cc4fb' +
                    '79d695bb732449f0e015745b86bfa371dc6ca7386e9c7309' +
                    '5549c2e4b8002873', 16);
-    assert(mr.test(p));
+    assert(miller.test(p));
   });
 });
